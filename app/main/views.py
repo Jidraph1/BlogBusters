51d28fe162ddd917a3d0cc5935ee0462f8398ae8
Jidraph! <jidraph.kimachia@student.moringaschool.com>
2022-05-15 19:58:26 +0300

    signup route

--- app/main/views.py
+++ app/main/views.py
@@ -12,6 +12,19 @@ def index():
     title= "BlogBusters"
     return render_template('index.html', title=title)
 
+@main.route('/login')
+def login():
+    
+    return render_template('login.html')
+
+
 @main.route('/')
 def signup():
-    asd
+    form = RegistrationForm()
+    if form.validate_on_submit():
+
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        return redirect('/success')
+    return render_template('signup.html')

4b65945b6b6cda7db2d9c31a7d471f877d879081
Jidraph! <jidraph.kimachia@student.moringaschool.com>
2022-05-16 20:27:42 +0300

    requests call

--- app/request.py
+++ app/request.py
@@ -16,3 +16,16 @@ def get_quote():
             quote_results_list = quote_data_response["quotes"]
             quote_results = process_results(quote_results_list)
     return quote_results
+
+def process_results(quote_list):
+    
+    quote_results = []
+    for quote_item in quote_list:
+        id = quote_item.get('id')
+        author = quote_item.get('author')
+        quote = quote_item.get('quote')
+   
+        quote_object=quote.Quote(id,author,quote)
+        quote_results.append(quote_object)
+        
+    return quote_results   

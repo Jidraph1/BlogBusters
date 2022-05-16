35d90b7815e4443337651d5cb4a58e9658d603b8
Jidraph! <jidraph.kimachia@student.moringaschool.com>
2022-05-16 20:17:28 +0300

    third migrate

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config:
 
 class ProdConfig(Config):
 
-    SQLALCHEMY_DATABASE_URI =os.environ.get('DATABASE_URL').replace("://", "ql://",1)
+    # SQLALCHEMY_DATABASE_URI =os.environ.get('DATABASE_URL').replace("://", "ql://",1)
     DEBUG = True
 
 class DevConfig(Config):
